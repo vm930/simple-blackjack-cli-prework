fe7d6c783e10d0bd88da7c2a1e42cfd2c02ec946
abstract-animation-9914 <vm930@hotmail.com>
2019-01-01 23:49:35 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -42,7 +42,7 @@ def hit?
   user_input = get_user_input
   if user_input === "h"
     deal_card
-  else user_input !== "h" || user_input !== "s"
+  else if user_input !== "h" || user_input !== "s"
     invalid_command
   end
 end
