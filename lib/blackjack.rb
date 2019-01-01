7bf206a984202d8b02f88684c6ede72d61eedd51
abstract-animation-9914 <vm930@hotmail.com>
2019-01-01 23:34:16 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -12,13 +12,10 @@ def display_card_total(num)
 end
 
 def prompt_user
-  # code #prompt_user here
   puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
-  prompt_user
   input = gets
 end
 
